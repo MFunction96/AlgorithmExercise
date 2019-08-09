1b8595a4f7829ddc84a583a6571d9e7b78befddf
MFunction <MFunction96@outlook.com>
2019-08-09 18:59:46 +0800

    Wrong Answer (Unknown)

--- src/UVa/401.cpp
+++ src/UVa/401.cpp
@@ -6,36 +6,21 @@
 
 #include <cstdio>
 #include <cstring>
+#include <cctype>
 
 const int MAXN = 300 + 10;
 char str[MAXN];
-bool ch[100];
+const char *rev = "A   3  HIL JM O   2TUVWXY51SE Z  8 ";
+const char *msg[] = {"not a palindrome.\n\n", "a regular palindrome.\n\n","a mirrored string.\n\n","a mirrored palindrome.\n\n"};
+
+char revchar(char c)
+{
+    return isalpha(c) ? rev[c - 'A'] : rev[c + 25 - '0'];
+}
 
 int main()
 {
-    freopen("401.in", "r", stdin);
-    memset(ch, 0, sizeof ch);
-    ch['A'] = true;
-    ch['E'] = true;
-    ch['H'] = true;
-    ch['I'] = true;
-    ch['J'] = true;
-    ch['L'] = true;
-    ch['M'] = true;
-    ch['O'] = true;
-    ch['S'] = true;
-    ch['T'] = true;
-    ch['U'] = true;
-    ch['V'] = true;
-    ch['W'] = true;
-    ch['X'] = true;
-    ch['Y'] = true;
-    ch['Z'] = true;
-    ch['1'] = true;
-    ch['2'] = true;
-    ch['3'] = true;
-    ch['5'] = true;
-    ch['8'] = true;
+    //freopen("401.in", "r", stdin);  
     while (scanf("%s", str) == 1)
     {
         printf("%s -- is ", str);
@@ -43,20 +28,17 @@ int main()
         auto flag = 3;
         for (auto i = 0;i < len && flag;i++)
         {
-            if (flag & 1 && str[i] != str[len - 1 - i])
+            if (str[i] != str[len - 1 - i])
             {
                 flag &= 2;
-            }
-            if (flag & 2 && !ch[str[i]])
-            {
-                flag &= 1;
+                if (str[i] != revchar(str[len - 1 - i]))
+                {
+                    flag &= 1;
+                }
             }
         }
-        if (flag == 0) printf("not a palindrome.\n\n");
-        else if (flag == 1) printf("a regular palindrome.\n\n");
-        else if (flag == 2) printf("a mirrored string.\n\n");
-        else printf("a mirrored palindrome.\n\n");
+        printf("%s", msg[flag]);
     }
-    freopen("CON", "r", stdin);
+    //freopen("CON", "r", stdin);
     return 0;
 }
