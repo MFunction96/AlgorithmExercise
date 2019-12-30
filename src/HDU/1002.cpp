85d5a3166926504e7d15d283a26211e59d5d4e45
MFunction96 <mfunction96@gmail.com>
2019-12-30 17:05:43 +0800

    HDU1002 Accepted

--- src/HDU/1002.cpp
+++ src/HDU/1002.cpp
@@ -37,31 +37,23 @@ int main()
             if (cnt > 1) printf("\nCase %d:\n", cnt);
             else printf("Case %d:\n", cnt);
             scanf("%s%s", a, b);
-            reverse(a);
-            reverse(b);
-            auto l1 = strlen(a);
-            auto l2 = strlen(b);
-            auto l = l1 < l2 ? l1 : l2;
+            memset(ans, 0 ,sizeof ans);
+            int l1 = strlen(a);
+            int l2 = strlen(b);
+            auto l = l1 > l2 ? l1 : l2;
             auto exceed = false;
             for (auto i = 0; i < l; ++i)
             {
-                auto tmp = a[i] + b[i] - '0' - '0';
+                auto i1 = l1 - 1 - i;
+                auto i2 = l2 - 1 - i;
+                auto n1 = (i1 < 0 ? 0 : a[i1] - '0');
+                auto n2 = (i2 < 0 ? 0 : b[i2] - '0');
+                auto tmp = n1 + n2;
                 if (exceed) ++tmp;
-                if (tmp > 9)
-                {
-                    exceed = true;
-                    tmp %= 10;
-                }
-                else exceed = false;
-                ans[i] = '0' + tmp;
+                exceed = tmp > 9;
+                ans[i] = tmp % 10 + '0';
             }
-            if (exceed) 
-            {
-                ans[l] = '1';
-                ans[l + 1] = 0;
-            }
-            reverse(a);
-            reverse(b);
+            if (exceed) ans[l] = '1';
             reverse(ans);
             printf("%s + %s = %s\n", a, b, ans);
         }
